78f6326c03b231ccbaa29b3c89cedf54c6ee3905
James Alexander Hughes <jameshughes89@Gmail.com>
2024-11-29 21:43:21 -0400

    write to file

--- site/topics/control-logic/create_control_logic_patterns_for_look_up_table.py
+++ site/topics/control-logic/create_control_logic_patterns_for_look_up_table.py
@@ -56,21 +56,43 @@ Mirocodes for the 16 possible instructions. Each microcode could be up to 8 inst
 5, thus, there is some "wasted" space in the lookup table. Not all 16 operations are implemented here. 
 """
 INSTRUCTIONS = [
-  [PCO|RMI,   RMO|IRI|PCE, 0,       0           ],  # 0b0000 --- 0x0 --- NOOP --- No Operation
-  [PCO|RMI,   RMO|IRI|PCE, IRO|ADR, RMO|ARI     ],  # 0b0001 --- 0x1 --- LDAR --- Load A From RAM
-  [PCO|RMI,   RMO|IRI|PCE, IRO|ARI, 0           ],  # 0b0010 --- 0x2 --- LDAD --- Load A Direct
-  [PCO|RMI,   RMO|IRI|PCE, IRO|ADR, RMO|BRI     ],  # 0b0011 --- 0x3 --- LDBR --- Load B From RAM
-  [PCO|RMI,   RMO|IRI|PCE, IRO|BRI, 0           ],  # 0b0100 --- 0x4 --- LDBD --- Load B Direct
-  [PCO|RMI,   RMO|IRI|PCE, IRO|ADR, ARO|RMI     ],  # 0b0101 --- 0x5 --- SAVA --- Save A to RAM
-  [PCO|RMI,   RMO|IRI|PCE, IRO|ADR, BRO|RMI     ],  # 0b0110 --- 0x6 --- SAVB --- Save B to RAM
-  [PCO|RMI,   RMO|IRI|PCE, ALU|ARI, 0           ],  # 0b0111 --- 0x7 --- ADDB --- Add to A
-  [PCO|RMI,   RMO|IRI|PCE, ALU|SUB|ARI,         ],  # 0b1000 --- 0x8 --- SUBB --- Subtract from A
-  [PCO|RMI,   RMO|IRI|PCE, IRO|PCI, 0           ],  # 0b1001 --- 0x9 --- JUMP --- Jump
-  [PCO|RMI,   RMO|IRI|PCE, 0,       0           ],  # 0b1010 --- 0xA --- NOOP --- No Operation
-  [PCO|RMI,   RMO|IRI|PCE, 0,       0           ],  # 0b1011 --- 0xB --- NOOP --- No Operation
-  [PCO|RMI,   RMO|IRI|PCE, 0,       0           ],  # 0b1100 --- 0xC --- NOOP --- No Operation
-  [PCO|RMI,   RMO|IRI|PCE, IRO|ADR, RMO|ORI     ],  # 0b1101 --- 0xD --- OUTU --- Output Unsigned Integer
-  [PCO|RMI,   RMO|IRI|PCE, IRO|ADR, RMO|ORI|SGN ],  # 0b1110 --- 0xE --- OUTS --- Output Signed Integer
-  [PCO|RMI,   RMO|IRI|PCE, HLT,     0           ],  # 0b1111 --- 0xF --- HALT --- Halt
+  [PCO|ADR,   RMO|IRI|PCE, 0,       0           ],  # 0b0000 --- 0x0 --- NOOP --- No Operation
+  [PCO|ADR,   RMO|IRI|PCE, IRO|ADR, RMO|ARI     ],  # 0b0001 --- 0x1 --- LDAR --- Load A From RAM
+  [PCO|ADR,   RMO|IRI|PCE, IRO|ARI, 0           ],  # 0b0010 --- 0x2 --- LDAD --- Load A Direct
+  [PCO|ADR,   RMO|IRI|PCE, IRO|ADR, RMO|BRI     ],  # 0b0011 --- 0x3 --- LDBR --- Load B From RAM
+  [PCO|ADR,   RMO|IRI|PCE, IRO|BRI, 0           ],  # 0b0100 --- 0x4 --- LDBD --- Load B Direct
+  [PCO|ADR,   RMO|IRI|PCE, IRO|ADR, ARO|RMI     ],  # 0b0101 --- 0x5 --- SAVA --- Save A to RAM
+  [PCO|ADR,   RMO|IRI|PCE, IRO|ADR, BRO|RMI     ],  # 0b0110 --- 0x6 --- SAVB --- Save B to RAM
+  [PCO|ADR,   RMO|IRI|PCE, ALU|ARI, 0           ],  # 0b0111 --- 0x7 --- ADDB --- Add to A
+  [PCO|ADR,   RMO|IRI|PCE, ALU|SUB|ARI,         ],  # 0b1000 --- 0x8 --- SUBB --- Subtract from A
+  [PCO|ADR,   RMO|IRI|PCE, IRO|PCI, 0           ],  # 0b1001 --- 0x9 --- JUMP --- Jump
+  [PCO|ADR,   RMO|IRI|PCE, 0,       0           ],  # 0b1010 --- 0xA --- NOOP --- No Operation
+  [PCO|ADR,   RMO|IRI|PCE, 0,       0           ],  # 0b1011 --- 0xB --- NOOP --- No Operation
+  [PCO|ADR,   RMO|IRI|PCE, 0,       0           ],  # 0b1100 --- 0xC --- NOOP --- No Operation
+  [PCO|ADR,   RMO|IRI|PCE, IRO|ADR, RMO|ORI     ],  # 0b1101 --- 0xD --- OUTU --- Output Unsigned Integer
+  [PCO|ADR,   RMO|IRI|PCE, IRO|ADR, RMO|ORI|SGN ],  # 0b1110 --- 0xE --- OUTS --- Output Signed Integer
+  [PCO|ADR,   RMO|IRI|PCE, HLT,     0           ],  # 0b1111 --- 0xF --- HALT --- Halt
 ]
 
+"""
+Write the microcode patterns for each instructions to a hex file. 
+
+Each row/individual microcode is accessed by some pattern inputted into the lookup table. The 4 most significant bits of
+the input pattern correspond to the specific instruction, and the last two bits corresponds to an individual microcode 
+for the corresponding instruction.
+
+  INSTRUCTION | MICROCODE
+      0 0 0 0 | 0 0 
+  
+For example, consider LDAR (Load A from RAM)
+
+      0 0 0 1 | 0 0 --- Program Counter Out + Address Register In 
+      0 0 0 1 | 0 1 --- RAM Out + Instruction Register In + Program Counter Enable
+      0 0 0 1 | 1 0 --- Instruction Register Out +  Address Register In 
+      0 0 0 1 | 1 1 --- RAM Out + A Register In 
+  
+"""
+with open("control_logic_patterns_for_look_up_table.hex", "w") as hex_file:
+  hex_file.write("v2.0 raw\n")
+  for instruction in INSTRUCTIONS:
+    hex_file.writelines(f"{hex(microcode_pattern)}\n" for microcode_pattern in instruction)
