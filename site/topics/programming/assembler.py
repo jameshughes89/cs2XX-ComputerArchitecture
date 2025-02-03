0ff5cf6887cefbf57d7a2cf18b24a93f0b0106d2
James Hughes <jameshughes89@gmail.com>
2025-02-03 14:22:59 -0400

    parse instructions

--- site/topics/programming/assembler.py
+++ site/topics/programming/assembler.py
@@ -27,12 +27,49 @@ HAS_OPERAND = {
     "OUTS",
 }
 
+
 def program_string_to_list_of_instructions(program:str) -> list[str]:
     program_list = program.strip().split("\n")
     if len(program_list) > 16:
         raise ValueError(f"Program length of {len(program_list)} exceeds maximum size of 16 bytes")
     return program_list
 
+
+def parse_instruction_operator(instruction:list[str]) -> int:
+    try:
+        operator = OPERATORS[instruction[0]]
+    except KeyError:
+        raise ValueError(f"Unknown operator {instruction[0]}")
+    return operator
+
+
+def parse_instruction_operand(instruction:list[str]) -> int:
+    if instruction[0] not in HAS_OPERAND:
+        if len(instruction) > 1:
+            raise ValueError(f"Operator {instruction[0]} requires no operand")
+        operand = 0
+    else:
+        if len(instruction) != 2:
+            raise ValueError(f"Operator {instruction[0]} requires one operand")
+        operand_string = instruction[1]
+        try:
+            if operand_string[:2] == "0b":
+                operand = int(operand_string, 2)
+            elif operand_string[:2] == "0x":
+                operand = int(operand_string, 16)
+            else:
+                operand = int(operand_string)
+        except ValueError:
+            raise ValueError(f"Cannot parse operand {operand_string}")
+        if operand >= 16:
+            raise ValueError(f"Operand value {operand_string} too large")
+    return operand
+
+
+def create_assembled_string(machine_codes: list[int]) -> str:
+    raw_line = "v2.0 raw\n"
+    assembled_program = "".join([f"0x{code:02x}\n" for code in machine_codes])
+    return raw_line + assembled_program
 
 
 if __name__ == "__main__":
@@ -42,37 +79,16 @@ if __name__ == "__main__":
         program_string = file.read()
 
     instruction_list = program_string_to_list_of_instructions(program_string)
-
-    
+    # PARSE A NUMBER!
 
     machine_code = [0x00] * 16
     for i, line in enumerate(instruction_list):
         instruction = line.split()
-        try:
-            operator = OPERATORS[instruction[0]]
-        except KeyError:
-            raise RuntimeError(f"Unknown operator {instruction[0]}")
-        if instruction[0] not in HAS_OPERAND:
-            if len(instruction) > 1:
-                raise RuntimeError(f"Operator {instruction[0]} requires no operand")
-            operand = 0
-        else:
-            if len(instruction) != 2:
-                raise RuntimeError(f"Operator {instruction[0]} requires one operand")
-            operand_string = instruction[1]
-
-            try:
-                if operand_string[:2] == "0b":
-                    operand = int(operand_string, 2)
-                elif operand_string[:2] == "0x":
-                    operand = int(operand_string, 16)
-                else:
-                    operand = int(operand_string)
-            except ValueError:
-                raise RuntimeError(f"Cannot parse operand {operand_string}")
-
+        operator = parse_instruction_operator(instruction)
+        operand = parse_instruction_operand(instruction)
         machine_code[i] = machine_code[i] | (operator << 4 | operand)
 
+
+    assembled_string = create_assembled_string(machine_code)
     with open("a.hex", "w") as hex_file:
-        hex_file.write("v2.0 raw\n")
-        hex_file.writelines([f"{hex(code):04}\n" for code in machine_code])
+        hex_file.write(assembled_string)
